3311bf3fa2e40413142fd45807840590f6a1c8d9
CesarOrtiz1994 <cesarortize94@gmail.com>
2023-07-26 12:59:08 -0600

    agregar todo

---
+++ src/models/pacientes.js
@@ -0,0 +1,20 @@
+const e = require('express');
+const mongoose = require('mongoose');
+
+//crear el esquema
+const pacientesSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    edad: {
+        type: Number,
+        required: true,
+    },
+    sangre: {
+        type: String,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('Paciente', pacientesSchema);
