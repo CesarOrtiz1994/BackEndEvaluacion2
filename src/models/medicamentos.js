8e32ccb7c121743d437c3cbf19ad75d3269cf8f9
zzALFREDzz\Uriel <urielbotelloresendiz@gmail.com>
2023-08-08 13:11:12 -0600

    Se quitan uppercase del modelo

--- src/models/medicamentos.js
+++ src/models/medicamentos.js
@@ -5,35 +5,28 @@ const medicineSchema = mongoose.Schema(
       nombre: {
         type: String,
         unique: true,
-        uppercase: true,
-        minlength: 6,
         maxlength: 100,
         required: true,
       },
       descripcion: {
         type: String,
         maxlength: 500,
-        uppercase: true,
       },
       dosisRec: {
         type: String,
         maxlength: 20,
-        uppercase: true,
         required: true,
       },
       efectosSec: {
         type: String,
         maxlength: 500,
-        uppercase: true,
       },
       contraindicaciones: {
         type: String,
         maxlength: 500,
-        uppercase: true,
       },
       fechaCaduc: {
         type: String,
-        uppercase: true,
         required: true,
       },
     }
